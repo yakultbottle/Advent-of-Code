788e40e92d7bb4dc303966632ceaab569eabe59e
yakultbottle <senorpotato8433@gmail.com>
2024-12-10 15:58:36 +0800

    Day 10 with part2 easier than part1

---
+++ day_10/part1.py
@@ -0,0 +1,40 @@
+import sys
+
+def read_data() -> list[list[int]]:
+    input = sys.stdin.readlines()
+    map = [[int(char) for char in string.strip()] for string in input]
+    return map
+
+def count_trails(map: list[list[int]], visited: set[tuple[int,int]], curr: int, x: int, y: int) -> int:
+    if curr == 9 and (x, y) not in visited:
+        visited.add((x, y))
+        return 1
+
+    directions = [(-1, 0), (0, 1), (0, -1), (1, 0)]
+    count = 0
+
+    for direction in directions:
+        dx, dy = direction
+        newX, newY = x + dx, y + dy
+
+        if not (0 <= newX < len(map[0]) and 0 <= newY < len(map)):
+            continue
+
+        if map[newY][newX] == curr + 1:
+            count += count_trails(map, visited, curr + 1, newX, newY)
+    return count
+
+def main(map: list[list[int]]) -> int:
+    count = 0
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == 0:
+                visited = set()
+                count += count_trails(map, visited, 0, j, i)
+
+    return count
+
+if __name__ == "__main__":
+    map = read_data()
+    output = main(map)
+    print(output)
