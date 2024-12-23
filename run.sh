28d0a2fcffb6dab4378e6c098b9efdde10a58c1f
yakultbottle <senorpotato8433@gmail.com>
2024-12-23 17:35:30 +0000

    Day 16, died bughunting p2

--- run.sh
+++ run.sh
@@ -8,12 +8,22 @@ fi
 
 # Map arguments to script and file names
 script="part$1.py"  # Converts 1/2 to part1.py/part2.py
-if [ "$2" == "small" ]; then
-    file="small_test.txt"
-elif [ "$2" == "test" ]; then
-    file="test.txt"
+if [[ "$2" == small* ]]; then
+    number=${2#small}
+    if [[ -z "$number" ]]; then
+	file="small_test.txt"
+    else
+        file="small_test${number}.txt"
+    fi
+elif [[ "$2" == test* ]]; then
+    number=${2#test}
+    if [[ -z "$number" ]]; then
+	file="test.txt"
+    else
+        file="test${number}.txt"
+    fi
 else
-    echo "Error: Second argument must be 'small' or 'test'."
+    echo "Invalid input: $2"
     exit 1
 fi
 
