3aa982f25b3cd9cb71f280b9f049a4767466b79e
yakultbottle <senorpotato8433@gmail.com>
2025-01-21 16:36:21 +0800

    2023 Day 9, easy

---
+++ 2023/day_9/part2.py
@@ -0,0 +1,25 @@
+input = open(0).read().strip().split("\n")
+input = [[int(num) for num in line.split()] for line in input]
+
+def next_num(line: list[int]) -> int:
+    differences = []
+    all_zero = True
+
+    i = 0
+    while i + 1 < len(line):
+        diff = line[i + 1] - line[i]
+        if diff != 0:
+            all_zero = False
+        differences.append(diff)
+        i += 1
+
+    if all_zero:
+        return line[0]
+
+    return line[0] - next_num(differences)
+
+answer = 0
+for line in input:
+    answer += next_num(line)
+
+print(answer)
