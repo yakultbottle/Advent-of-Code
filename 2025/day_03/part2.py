b5956b4ced20f31f4285f3f8de76837a87bccc50
yakultbottle <benjaminlwm7983@gmail.com>
2025-12-04 14:29:42 +0800

    Implement monotonic stack, not faster on test set

--- 2025/day_03/part2.py
+++ 2025/day_03/part2.py
@@ -1,24 +1,43 @@
-banks = open(0).read().strip().split("\n")
+# banks = open(0).read().strip().split("\n")
 
-def largest(bank: str, start: int, end: int) -> int:
-    maximum = 0
-    index = -1
-    for i in range(start, end):
-        num = int(bank[i])
-        if num > maximum:
-            maximum = num
-            index = i
-    return index
+# def largest(bank: str, start: int, end: int) -> int:
+#     maximum = 0
+#     index = -1
+#     for i in range(start, end):
+#         num = int(bank[i])
+#         if num > maximum:
+#             maximum = num
+#             index = i
+#     return index
 
+# ans = 0
+# for bank in banks:
+#     n = len(bank)
+#     start = 0
+#     maximum = []
+#     for i in range(11, -1, -1):
+#         index = largest(bank, start, n - i)
+#         maximum.append(bank[index])
+#         start = index + 1
+#     ans += int("".join(maximum))
+
+# print(ans)
+
+# Monotonic stack solution, hoped to be faster but wasn't
+banks = [list(map(int, bank)) for bank in open(0).read().strip().split("\n")]
+n = len(banks[0])
 ans = 0
-for bank in banks:
-    n = len(bank)
-    start = 0
-    maximum = []
-    for i in range(11, -1, -1):
-        index = largest(bank, start, n - i)
-        maximum.append(bank[index])
-        start = index + 1
-    ans += int("".join(maximum))
 
+for bank in banks:
+    temp = []
+    for idx, num in enumerate(bank):
+        while temp and temp[-1] < num and n - idx > 12 - len(temp):
+            temp.pop()
+        if len(temp) >= 12:
+            continue
+
+        temp.append(num)
+
+    something = int("".join([str(x) for x in temp]))
+    ans += something
 print(ans)
