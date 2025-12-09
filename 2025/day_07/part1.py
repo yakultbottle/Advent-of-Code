64d94bf86882d6d44e49c54f5779dacaf275b9da
yakultbottle <benjaminlwm7983@gmail.com>
2025-12-09 02:38:50 +0000

    Implement day 7 Solution was easy, got spoiled though

---
+++ 2025/day_07/part1.py
@@ -0,0 +1,38 @@
+grid = [list(row) for row in open(0).read().strip().split("\n")]
+
+height, width = len(grid), len(grid[0])
+
+start = (-1, -1)
+for y in range(height):
+    for x in range(width):
+        if grid[y][x] == "S":
+            start = (x, y)
+            break
+
+frontier = [start]
+seen = set(start)
+splitters = set()
+
+while frontier:
+    x, y = frontier.pop()
+    
+    if grid[y][x] == "^":
+        splitters.add((x, y))
+        for dx, dy in ((x - 1, y), (x + 1, y)):
+            if not (dx in range(width) and dy in range(height)):
+                continue
+            if (dx, dy) in seen:
+                continue
+            seen.add((dx, dy))
+            frontier.append((dx, dy))
+    else:
+        dx, dy = x, y + 1
+        if not (dx in range(width) and dy in range(height)):
+            continue
+        if (dx, dy) in seen:
+            continue
+        seen.add((dx, dy))
+        frontier.append((dx, dy))
+
+print(len(splitters))
+
