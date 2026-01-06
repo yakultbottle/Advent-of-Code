b0702166fb484718869743ac6c40763a6fbd937a
yakultbottle <benjaminlwm7983@gmail.com>
2026-01-06 23:40:37 +0100

    Improve part 1

--- 2025/day_10/part1.py
+++ 2025/day_10/part1.py
@@ -1,31 +1,29 @@
+from itertools import combinations
+
 input = open(0).read().strip().split("\n")
 
 ans = 0
 for row in input:
     first, *second, _ = row.split()
-    end_state = [char == "#" for char in first[1:-1]]
-    switches = [list(map(int, switch[1:-1].split(","))) for switch in second]
+    end_state = sum(1 << i for i, char in enumerate(first[1:-1]) if char == "#")
+    switches = [sum(1 << i for i in map(int, switch[1:-1].split(","))) for switch in second]
 
-    curr_state = [False for _ in range(len(end_state))]
-    n = len(switches)
+    num = -1
+    for r in range(len(switches) + 1):
+        for combi in combinations(switches, r):
+            curr_state = 0
+            for switch in combi:
+                curr_state ^= switch
 
-    def knapsack(idx: int, count: int) -> int:
-        if curr_state == end_state:
-            return count
-        if idx >= n:
-            return float('inf')
+            if curr_state == end_state:
+                break
+        else:
+            continue
+        num = r
 
-        minSoFar = float('inf')
-        for i in range(idx, n):
-            for switch in switches[i]:
-                curr_state[switch] = not curr_state[switch]
-            take = knapsack(i + 1, count + 1)
-            for switch in switches[i]:
-                curr_state[switch] = not curr_state[switch]
-            dontTake = knapsack(i + 1, count)
-            minSoFar = min(minSoFar, take, dontTake)
-        return minSoFar
+    if num == -1:
+        print("what")
 
-    ans += knapsack(0, 0)
+    ans += num
 
 print(ans)
